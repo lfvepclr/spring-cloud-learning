311a8451832684b3560ac6cec1c4946439a6a912
bestwishes2you@yeah.net <LFVEPHHY1002>
2017-06-01 15:22:05 +0800

    [lofo]config server & eureka-server & user provider

--- configserver/src/main/java/lofo/spring/cloud/config/server/App.java
+++ configserver/src/main/java/lofo/spring/cloud/config/server/App.java
@@ -1,4 +1,4 @@
-package lofo.spring.cloud.service.user.config.server;
+package lofo.spring.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
