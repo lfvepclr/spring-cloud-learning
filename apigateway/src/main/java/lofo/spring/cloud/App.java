12854f5772a4f650f3bd7a758e6c8c68f12544ab
bestwishes2you@yeah.net <LFVEPHHY1002>
2017-06-02 09:31:30 +0800

    [lofo]config server & eureka-server & user provider

---
+++ apigateway/src/main/java/lofo/spring/cloud/App.java
@@ -0,0 +1,13 @@
+package lofo.spring.cloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
