11ccef51c0715daa09f52389a49f49b97c9599c8
lofo <lfvepclr@yeah.net>
2017-06-02 12:05:23 +0800

    [lofo]api-gateway

--- apigateway/src/main/java/lofo/spring/cloud/App.java
+++ apigateway/src/main/java/lofo/spring/cloud/App.java
@@ -1,13 +1,17 @@
 package lofo.spring.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
